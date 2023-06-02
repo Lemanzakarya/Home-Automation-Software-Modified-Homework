ee7c5be9bff409a394be07a6ac1cefdf0b51cc45
Lemanzakarya <95051873+Lemanzakarya@users.noreply.github.com>
2023-06-02 19:25:55 +0400

    Buttons arranged on "Producer" page

--- producer/main.js
+++ producer/main.js
@@ -1,6 +1,12 @@
 const allButtons = document.querySelectorAll(".circular-button")
-allButtons.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        item.classList.toggle("btn-on")
+allButtons.forEach((item) => {
+    const btnText = item.querySelector(".btn-status")
+    item.addEventListener("click", () => {
+        item.classList.toggle("btn-on");
+        if(item.classList.contains("btn-on")) {
+            btnText.innerHTML = "ON"
+        } else {
+            btnText.innerHTML = "OFF"
+        }
     })
 })
