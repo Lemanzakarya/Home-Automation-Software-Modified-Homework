f51ff5525e600b97e6de3f5f2ba1351d17ce0d4a
Lemanzakarya <95051873+Lemanzakarya@users.noreply.github.com>
2023-06-04 18:53:33 +0400

    main.js edited and buttons enabled

--- producer/main.js
+++ producer/main.js
@@ -1,12 +1,71 @@
 const allButtons = document.querySelectorAll(".circular-button")
-allButtons.forEach((item) => {
-    const btnText = item.querySelector(".btn-status")
-    item.addEventListener("click", () => {
-        item.classList.toggle("btn-on");
-        if(item.classList.contains("btn-on")) {
-            btnText.innerHTML = "ON"
+
+const btnObj = []
+
+window.onload = () => {
+    const allBtn = getAllBtnLocalStoreg();
+    allBtn.forEach((item, index) => {
+        const btn = document.querySelector(`[title="${item.title}"]`)
+        const btnText = btn.querySelector(".btn-status")
+        if (item.status === "ON") {
+            btn.classList.add("btn-on")
+            btnText.textContent = "ON"
         } else {
-            btnText.innerHTML = "OFF"
+            btn.classList.remove("btn-on")
+            btnText.textContent = "OFF"
         }
+    }
+    )
+}
+
+
+allButtons.forEach((item, index) => {
+    const btnText = item.querySelector(".btn-status")
+    item.addEventListener("click" ,() => {
+        clickedButton(item, btnText,index)
     })
 })
+console.log(btnObj)
+
+
+
+function clickedButton(item, btnText,index) {
+    item.classList.toggle("btn-on");
+    if (btnText.textContent === "OFF") {
+        item.classList.contains("btn-on")
+        btnText.textContent = "ON"
+    } else {
+        item.classList.contains("btn-on")
+        btnText.textContent = "OFF"
+    }
+    changeLocal(item, btnText,index)
+}
+
+function changeLocal(item, btnText,index) {
+    const allBtn = getAllBtnLocalStoreg();
+    const btnObj = {
+        id:index,
+        title: item.title,
+        status: btnText.textContent
+    }
+    allBtn.forEach((item, index) => {
+        if (item.title === btnObj.title) {
+            allBtn.splice(index, 1)
+        }    
+    })
+    allBtn.push(btnObj)
+    localStorage.setItem("btnStatus", JSON.stringify(allBtn))
+}
+
+
+function getAllBtnLocalStoreg() {
+    let btnStatus;
+    if (localStorage.getItem("btnStatus") === null) {
+        btnStatus = []
+    } else {
+        btnStatus = JSON.parse(localStorage.getItem("btnStatus"))
+    }
+    return btnStatus
+}
+
+
