a2740416624bed8554dd88826b6f3efb47f746b2
Lemanzakarya <95051873+Lemanzakarya@users.noreply.github.com>
2023-06-11 22:38:33 +0400

    changes

--- login/consumer_login.php
+++ login/consumer_login.php
@@ -3,22 +3,32 @@ session_start();
 
 @include 'config.php';
 
+$errors = [];
+
 if(isset($_POST['submit'])){
-    $name =  isset($_POST['name']) ? mysqli_real_escape_string($conn, $_POST['name']) : '';
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pass = md5($_POST['password']);
-    $cpass = isset($_POST['cpassword']) ? md5($_POST['cpassword']) : '';
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($conn, $_POST['email']) : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-    $select = "SELECT * FROM users WHERE email = '$email' && password = '$pass' ";
-    $result = mysqli_query($conn, $select);
+    $select = "SELECT * FROM users WHERE email = ? LIMIT 1";
+    $stmt = $conn->prepare($select);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    if(mysqli_num_rows($result) > 0){
-     header('location: ..\consumer\consumer.html');
-    }else{
-        $error[] = 'incorrect email or password!';
+    if($result->num_rows === 1){
+        $row = $result->fetch_assoc();
+        if(password_verify($password, $row['password'])){
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['email'] = $row['email'];
+            header('Location: ../consumer/consumer.html');
+            exit();
+        } else {
+            $errors[] = 'Incorrect email or password!';
+        }
+    } else {
+        $errors[] = 'Incorrect email or password!';
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -28,10 +38,7 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consumer Login Form</title>
-
-    
     <link rel="stylesheet" href="style1.css">
-
 </head>
 <body>
 <div class="logout-container">
@@ -42,10 +49,10 @@ if(isset($_POST['submit'])){
 
     <div class="form-container">
         <form action="" method="post">
-            <h3>login</h3>
+            <h3>Login</h3>
             <?php 
-            if(isset($error)){
-                foreach($error as $error){
+            if(!empty($errors)){
+                foreach($errors as $error){
                     echo '<span class="error-msg">'.$error.'</span>';
                 }
             }
@@ -54,8 +61,8 @@ if(isset($_POST['submit'])){
             <input type="email" name="email" required placeholder="Enter your email">
             <input type="password" name="password" required placeholder="Enter your password">
             <input type="submit" name="submit" value="Login" class="form-btn">
-            <p>don't have an account? <a href="consumer_register.php">sign up</a></p>
-   </form>
- </div>
+            <p>Don't have an account? <a href="consumer_register.php">Sign up</a></p>
+        </form>
+    </div>
 </body>
 </html>
