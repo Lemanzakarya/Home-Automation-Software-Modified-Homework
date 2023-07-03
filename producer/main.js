f19af259c76651d29e6f96be91411819e5d1f734
ozzlems <ozlemsencoruh@gmail.com>
2023-07-03 17:38:03 +0300

    commit

--- producer/main.js
+++ producer/main.js
@@ -1,4 +1,26 @@
 const allButtons = document.querySelectorAll(".circular-button")
+let selectedRoomId = '';
+
+function deleteRoom(roomId) {
+  selectedRoomId = roomId;
+  // Add any additional logic for deleting a room
+}
+
+function showConfirmation() {
+  if (selectedRoomId) {
+    const selectedRoom = document.getElementById(selectedRoomId);
+    const roomName = selectedRoom.innerText.trim();
+    const confirmation = confirm(`Are you sure you want to delete ${roomName}?`);
+
+    if (confirmation) {
+      // Call the deleteRoom function to delete the selected room
+      deleteRoom(selectedRoomId);
+      selectedRoom.remove();
+      selectedRoomId = '';
+    }
+  }
+}
+
 
 const btnObj = []
 
@@ -76,10 +98,24 @@ function openPopup() {
     document.getElementById("popupContainer").style.display = "none";
   }
   
-  document.getElementById("roomForm").addEventListener("submit", function (e) {
-    e.preventDefault(); 
+  function showDevicePopup() {
+    document.getElementById("devicePopup").style.display = "block";
+  }
+ 
+  
+  function cancelDevice() {
+    document.getElementById("devicePopup").style.display = "none";
+  }
+  
+  function addDevice() {
+    const deviceName = document.getElementById("deviceName").value;
     const roomName = document.getElementById("roomName").value;
-    console.log("Room Name:", roomName);
-    closePopup(); 
-  });
+  
+    // Perform the desired operation here (e.g., adding the device or any other action)
+  
+    console.log("Device Name: ", deviceName);
+    console.log("Room Name: ", roomName);
+  
+    document.getElementById("devicePopup").style.display = "none";
+  }
   
